3e845f126a2ac7390495ee4c48bcfcdb2d75dada
thiagosoares-mobile <thiago.soares.jr@gmail.com>
2017-08-09 17:42:36 -0300

    Ajustes de Main layout

---
+++ src/main/webapp/app/layouts/sidemenu/sidemenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'jhi-side-menu',
+    templateUrl: './sidemenu.component.html'
+})
+export class SideMenuComponent implements OnInit {
+
+    constructor(
+    ) {}
+
+    ngOnInit() {
+
+    }
+}
